a9badf2907820a21e2a1f8620595a5c7d9bb0675
Gustaf Starkenberg <gustaf@gspmedia.se>
2026-08-03 03:27:03 +0200

    Re-saved server.js

--- server.js
+++ server.js
@@ -10,3 +10,9 @@ const server = http.createServer(app);
 
 // Static files (widget)
 app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Discord backend running on port " + PORT);
+});
