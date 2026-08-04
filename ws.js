d81cda7bf30558230abbee18a3e60dcc874ea6cd
Gustaf Starkenberg <gustaf@gspmedia.se>
2026-08-04 18:32:48 +0200

    Debug live reactions

--- ws.js
+++ ws.js
@@ -42,6 +42,7 @@ function createWSServer(server, client) {
 
   // Reactions från Discord → skickas till frontend
   client.on("messageReactionAdd", (reaction, user) => {
+    console.log("[REACTION]", reaction.emoji.name, reaction.message.id);
     wss.broadcast({
       type: "reaction",
       channel: reaction.message.channel.id,
